5eb9a5de654937a7fe308f4a534ff1f3ff884338
rishi <kumarcrishikesh@gmail.com>
2024-11-19 09:08:21 -0500

    WIP: Select 1 works.

--- src/main/scala/com/neu/Main.scala
+++ src/main/scala/com/neu/Main.scala
@@ -1,7 +1,27 @@
-package com.neu
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import com.crobox.clickhouse.ClickhouseClient
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-object Main {
+object ClickhouseExample {
   def main(args: Array[String]): Unit = {
-    println("hello world")
+    implicit val system: ActorSystem = ActorSystem("clickhouse-example")
+
+    val config =ConfigFactory.load()
+
+    val client = new ClickhouseClient(Some(config))
+
+    val query: Future[String] = client.query("SELECT 1")
+
+    query.map { result =>
+      println(s"Query result: $result")
+    }.recover {
+      case e: Exception => println(s"Query failed: ${e.getMessage}")
+    }.onComplete { _ =>
+      system.terminate()
+    }
+
+    Thread.sleep(5000)
   }
 }
