592fe2d4058b80620d8a3fdb68b59d08199aea80
Rishikesh Kumar C <rkc@localhost.localdomain>
2024-12-06 21:45:13 -0500

    minor refactor of CKHConnector

--- src/main/scala/com/neu/connectors/CKHConnector.scala
+++ src/main/scala/com/neu/connectors/CKHConnector.scala
@@ -3,20 +3,17 @@ package com.neu.connectors
 import org.apache.pekko.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import com.crobox.clickhouse.ClickhouseClient
-import com.crobox.clickhouse.stream.ClickhouseSink
 
 
-import scala.concurrent.{ExecutionContext, Future}
 
-
-class CKHConnector(implicit system: ActorSystem, ec: ExecutionContext) {
+class CKHConnector() {
   private val config = ConfigFactory.load()
   val client = new ClickhouseClient(Some(config))
 }
 
 
 object CKHConnector {
-  def apply()(implicit system: ActorSystem, ec: ExecutionContext): CKHConnector = {
+  def apply(): CKHConnector = {
     new CKHConnector()
   }
 }
